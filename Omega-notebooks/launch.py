836051c7486cdc4d5fb79f5119f034caade12d88
SepidehNahali <109043122+SepidehNahali@users.noreply.github.com>
2022-07-12 15:48:27 -0400

    Update launch.py

--- Omega-notebooks/launch.py
+++ Omega-notebooks/launch.py
@@ -18,7 +18,7 @@ def exp1(name):
     num_machines_per_rack = 4
     num_racks_per_cluster = 2
     max_gpu_request = 8
-    max_job_len = 30
+    max_job_len = 20
     jobqueue_maxlen = 100
     max_backlog_len = 10
     new_job_rate = 0.6
