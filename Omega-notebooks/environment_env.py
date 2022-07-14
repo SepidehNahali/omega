9dd2c11b633333661ca881a46c4e478b184338b1
SepidehNahali <109043122+SepidehNahali@users.noreply.github.com>
2022-07-14 10:12:30 -0400

    Update environment_env.py

--- Omega-notebooks/environment_env.py
+++ Omega-notebooks/environment_env.py
@@ -33,6 +33,9 @@ from matplotlib import pyplot as plt
 ##############################
 
 
+__all__ = ['Env']
+
+
 class Env(ABC):
     """Metaclass for defining customized environments.
 
@@ -95,7 +98,6 @@ class Env(ABC):
         Get k random avalible gpus
 
     """
-
     def __init__(self, pa):
         """Constructor of the environment
 
@@ -107,13 +109,12 @@ class Env(ABC):
         self.done = False
 
         self.pa = pa
-
         # Host class, contains gpu
         self.resources = -np.ones((self.pa.num_racks_per_cluster, self.pa.num_machines_per_rack,
                                    self.pa.num_gpus_per_machine), dtype=np.int32)  # Hossein
         self.gpus_array = np.array([], dtype=Gpu)
         # self.resources[:] = -1 # Hossein
-
+        
         # self.jobqueue = [None] * self.pa.jobqueue_maxlen # Hossein
         # self.jobqueue = np.full(self.pa.jobqueue_maxlen, None, dtype=np.object) # Hossein
 
@@ -122,18 +123,28 @@ class Env(ABC):
 
         self.jobqueue_maxlen = pa.jobqueue_maxlen
         self.j_id = 0
+        self.last_job_id = 0
         self.total_step = 0
         self.num_job_finished = 0
 
-        self.datagenerator = DataGenerating(max_gpu_request=pa.max_gpu_request, max_len=pa.max_job_len,
-                                            resources=self.resources,
-                                            ret_reducer=pa.ret_reducer)
+        self.datagenerator = DataGenerating(max_gpu_request=pa.max_gpu_request, max_len=pa.max_job_len,resources=self.resources,ret_reducer=pa.ret_reducer)
         self.len_seq = np.array([])
         self.gpu_request_seq = np.array([])
         self.gpus_per_rack = self.resources.shape[1] * self.resources.shape[2]
         self.curr_time = 0
         self.episode_reward = np.array([])
-
+        self.topology_parameters = NetworkTopology()
+        # self.topology_parameters.node_index_build()
+        # self.topology_parameters.create_graph()
+        # self.topology_parameters.create_adjacency_matrix()
+        # self.topology_parameters.get_gpu_distance_from_all(1)
+        # self.topology_parameters.get_gpu_sort_nearest(2)
+        # print('here is env total gpu nymbers: ', topology_parameters.gpu_number)
+        # for cluster in topology_parameters.cluster_gpus:
+        #     print('cluster_gpus: ',cluster.gpu_id)
+        # print('im in environment class here is the parameters: ',topology_parameters.get_gpu_sort_nearest(6))
+        self.assigned_gpu_to_jobs = {}
+        
     def reset(self):
         """Reset the environment
 
@@ -329,8 +340,7 @@ class Env(ABC):
     def advance_runningjobs_onestep(self):
         gpus_per_rack = self.resources.shape[1] * self.resources.shape[2]
         for job in self.jobqueue[self.get_running_jobs()]:
-            job.v_m, job.rt_m = calc_job_minbatch_speed(job, gpus_per_rack, self.pa.ret_reducer, singleormulti='multi',
-                                                        outdetails=True)
+            job.v_m, job.rt_m = calc_job_minbatch_speed(job, gpus_per_rack, self.pa.ret_reducer, singleormulti='multi',outdetails=True)
             d_delta = self.datagenerator.jobadvancecompdist(job)
             job.d_done += d_delta
             job.stepcounter += 1
@@ -364,12 +374,21 @@ class Env(ABC):
         """
         if all(x == -1 for x in self.resources[gpus]):
             self.resources[gpus] = self.jobqueue[j_idx].job_id
+            # print('job ',self.jobqueue[j_idx].job_id,' Just assigend  cordination: ',gpus )
+
+            # print('Job id: ',j_idx)
             self.jobqueue[j_idx].status = 'running'
             self.jobqueue[j_idx].gpus = gpus
+            # print('self.resources : ',self.resources)
+            # for ii in range(len(self.jobqueue)):
+            #     print('Job Queue: ',self.jobqueue[ii].job_id)
+
             return True
         return False
 
     def random_select_k_gpus_for_job(self, j):
+        # print('This is random_select_k_gpus_for_job: ')
+
         """Randomly select k gpus for the target job j
 
         Parameters
@@ -382,16 +401,31 @@ class Env(ABC):
         selected gpus : (ndarray, ndarray)
             A set of selected gpus
         """
+   
         if j < len(self.jobqueue):
             gpu_request = self.jobqueue[j].gpu_request
             avl_gpus = self.get_avl_gpus()
+            # print('avl_gpus[0]),len(avl_gpus[0]),gpu_request',avl_gpus[0],len(avl_gpus[0]),gpu_request)
             if len(avl_gpus[0]) >= 1 and len(avl_gpus[0]) >= gpu_request:
+               
+                # print('self.get_random_gpus(gpu_request): ',gpu_request,self.get_first_k_gpus(gpu_request))
+                # print('1- random_select_k_gpus_for_job: ',j ,len(self.jobqueue),avl_gpus[0],len(avl_gpus[0]))
                 return self.get_first_k_gpus(gpu_request)
+
             else:
+                # print('2- random_select_k_gpus_for_job: ',j ,len(self.jobqueue),avl_gpus[0],len(avl_gpus[0]))
+                # print('self.resources ',self.resources)
+
+                # return ([], [], [], [], [])
                 return ([], [], [])
+
         else:
+            # return ([], [], [], [], [])
+            print('3- random_select_k_gpus_for_job: ',j ,len(self.jobqueue),avl_gpus[0],len(avl_gpus[0]))
+
             return ([], [], [])
 
+   
     def get_avl_gpus(self, *c_idx):
         """Find all available gpus in the environment
 
@@ -467,10 +501,22 @@ class Env(ABC):
         """
 
         def getter(j):
+            # print('total computational distance of the job: ',j.job_id,' is equal to: ', j.d_f)
+            # print('and','j.d_done is: ',j.d_done )
             return j.d_done >= j.d_f
 
         vfunc = np.vectorize(getter, otypes=[bool])
+        # print('done jobs vfunc: ',vfunc )
+
         done_jobs = np.where(vfunc(self.jobqueue))
+
+        # print('done jobs: ',done_jobs)
+        # print('done jobs len: ',len(done_jobs))
+
+
+        # print('done_jobs[0],done_jobs[1],done_jobs[2] : ',done_jobs[0],done_jobs[1],done_jobs[2])
+
+
         return done_jobs
 
 
@@ -487,28 +533,122 @@ class Env(ABC):
         gpus : tuple
             A tuple represents the coordinates of selected gpus.git status
         """
-        if random:
-            x0 = self.get_avl_gpus()[0][:k]
-            x1 = self.get_avl_gpus()[1][:k]
-            x2 = self.get_avl_gpus()[2][:k]
-        else:
-            x0 = self.get_avl_gpus()[0][:k]
-            x1 = self.get_avl_gpus()[1][:k]
-            x2 = self.get_avl_gpus()[2][:k]
 
+        # print('This is get_random_gpus: ')
 
-        return (x0, x1, x2)
+        """Get random k available gpus
+
+        Parametersgit
+        ---------
+        k : int
+            The number of gpus that would like to pick up.
+
+        Returns
+        -------
+        gpus : tuple
+            A tuple represents the coordinates of selected gpus.git status
+        """
+        def get_kth_dims(f):#returns the Kth element dimentions in the 3-darray of self.resources
+            assert(f<32)
+            if f==3:
+               return 0,0,3
+            if f==6:
+               return 0,1,2
+            if f==7:
+               return 0,1,3
+            if f==11:
+               return 0,2,3
+            if f<=15:
+                x=0
+                y = round(f/4)
+                if(y>=4):
+                  y=3
+                z = f - 4*y
+            if f>15:
+                x=1
+                f=f-16
+                y=int(np.floor(f/4))
+                if(y>=4):
+                  y=3
+                z= f- 4*y
+            return x,y,z
+        first_avl_gpus = self.get_avl_gpus()
+
+        # x0 = self.get_avl_gpus(0)[0][:k] # first k index number of the first dim of resources(num of racks)
+        # x1 = self.get_avl_gpus(0)[1][:k] # first k index number of the second dim of resources(machine per rack)
+        # x2 = self.get_avl_gpus(0)[2][:k] # first k index number of the third dim of resources(gpu per machine)
+        
+        x_ = int(self.get_avl_gpus()[0][:1])####find the first idle gpu as before(first dim)
+        y_ = int(self.get_avl_gpus()[1][:1])####find the first idle gpu as before(second dim)
+        z_ = int(self.get_avl_gpus()[2][:1])####find the first idle gpu as before(third dim)
+    
+        WIDTH = 2 #(machine per rack)
+        DEPTH = 4 #(gpu per machine)
+        HEIGHT = 4 
+        x0 = np.where(self.resources == -1)[0][:0]# create three empty array to put the nearest gpu dims in them
+        x1 = np.where(self.resources == -1)[1][:0]
+        x2 = np.where(self.resources == -1)[2][:0]#(by appending will be extended to (k,) or np.where(self.resources == -1)[2][:k])
+
+        first_gpu_Id = x_ * HEIGHT * DEPTH + y_ * DEPTH + z_# get the first gpu number dims found randomly in(x,y,z)
+
+        get_nearest = self.topology_parameters.get_gpu_sort_nearest(first_gpu_Id)#search nearest gpus to it
+        # x0=np.append(x0,x_)# append the  to x0 the first gpu
+        # x1=np.append(x1,y_)# append the  to x1 the first gpu
+        # x2=np.append(x2,z_)# append the  to x2 the first gpu
+        idleneighbour=0
+        for i in range(len(get_nearest)-1):# for each neighbor gpu ID:[3,34,2,5],len=3,i=1,4
+            x,y,z= get_kth_dims(get_nearest[i])# tell the dims in resources matrice
+            if (self.resources[x][y][z]==-1): # to make sure that the neighbouring GPUs are idle
+                idleneighbour=idleneighbour+1
+                if (idleneighbour>k):#find the rest k-1 gpus from the sorted list
+                    # print('get_kth_dims:    ',get_kth_dims)
+                    break
+                x0=np.append(x0,x)# append the  to x0 which contains the k nearest gpus first dims
+                x1=np.append(x1,y)# append the  to x0 which contains the k nearest gpus second dims
+                x2=np.append(x2,z)# append the  to x0 which contains the k nearest gpus third dims
+        # print('first_gpu_Id:',first_gpu_Id,'get_nearest: ',get_nearest)
+
+        return (x0, x1, x2)# before x0=np.where(self.resources == -1)[0][:k],...  had the first k gpus in resources marice. now it has nearest k gpus in terms of topology
 
 
 
     def reward_throughput(self):
         def _log(x):
             if x <= 0:
-                return 0.01
+               return 0.01
             else:
                 return x
-        reward = 0
         total_waiting = 0
+        reward = 0
+        # print('here is env, reward funcion the job is :',self.joboo,' the gpus are: ',self.gpuu,' the gpus type: ',type(self.gpuu))
+        # print('here is env, reward funcion availible gpus :',self.get_avl_gpus())
+        # print('here is env, reward funcion all resources :',self.resources)
+        # print('here is env, reward funcion resources of cluster:',clus,' : ',self.resources[np.array(clus)])
+        # print('self.assigned_gpu_to_jobs: ',get_assigned_gpu_job_dict())
+        distancefromothers=0
+        topology_parameters = self.topology_parameters
+
+        # for j in self.jobqueue[self.get_running_jobs()]:
+        #     # print('im in reward loop, running job is: ',j.job_id)
+        #     for gp in j.gpus:
+        #         # print('list of gpus of the job: ',gp)
+        #         if(len(gp)>1):
+        #             # print('length of gpu array: ',len(gp))
+        #             nearest = topology_parameters.get_gpu_sort_nearest(gp[0]) #containing the first node itself
+        #             a=gp.tolist() # we could use (==).all in if statement compariation for np array too
+        #             a.sort()
+        #             b=nearest[:len(gp)].tolist()
+        #             b.sort() # index is because selecting first n of needed gpus from nearest gpus list
+        #             # topology_parameters.get_gpu_sort_nearest(gp[0])
+        #             for i in gp:
+        #                 distancefromothers += topology_parameters.get_gpu_distance_gpu(gp[0],i)
+        #             if(a==b): # sort nearest gpus to the first gpu and selected gpus to compare if they are optimal
+        #                 reward *= distancefromothers
+        #             else:
+        #                 reward /= distancefromothers
+                   
+
+
         for j in self.jobqueue[self.get_running_jobs()]:
             reward += j.v_m
             if len(j.gpus[0]) != j.gpu_request:
@@ -522,4 +662,8 @@ class Env(ABC):
         #     reward += calc_job_minbatch_speed(job=j, gpus_per_rack=self.gpus_per_rack, ret_reducer=self.pa.ret_reducer, singleormulti='single') * self.pa.dismiss_penalty
         reward = np.clip(reward, -300, 100)
         return reward
+
+
+
+
 
