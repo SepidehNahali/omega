b76f7abebb0c4bf0246883a0989e5b2342bec786
SepidehNahali <109043122+SepidehNahali@users.noreply.github.com>
2022-07-20 10:53:55 -0400

    Update environment_env.py

--- Omega-notebooks/environment_env.py
+++ Omega-notebooks/environment_env.py
@@ -662,8 +662,8 @@ class Env(ABC):
             reward += calc_job_minbatch_speed(job=j, gpus_per_rack=self.gpus_per_rack, ret_reducer=self.pa.ret_reducer, singleormulti='single') * self.pa.dismiss_penalty
         
         
-#         reward*=32-len(np.where(self.resources == -1)[0])
-        reward = np.clip(reward, -300, 100)
+        reward*=32-len(np.where(self.resources == -1)[0])
+#         reward = np.clip(reward, -300, 100)
 
         
         
