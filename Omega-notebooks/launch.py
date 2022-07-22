27b37de3d2fe137183b6fc032ab9a181c9151195
SepidehNahali <109043122+SepidehNahali@users.noreply.github.com>
2022-07-22 17:57:29 -0400

    Update launch.py

--- Omega-notebooks/launch.py
+++ Omega-notebooks/launch.py
@@ -140,7 +140,7 @@ parser.add_argument('-n',
 #         print("CUDA Device: %d" % torch.cuda.current_device())
 # if vars(args)['e'] == 1:
 # exp1(vars(args)['n'])
-exp1('Third')
+exp1('Fourth')
 # train(env)
 
 # if __name__ == '__main__':
