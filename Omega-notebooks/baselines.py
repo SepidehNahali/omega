169767b4b3762cec373ca6c511139e56f5b4c15d
SepidehNahali <109043122+SepidehNahali@users.noreply.github.com>
2022-08-15 16:08:16 -0400

    Update baselines.py

--- Omega-notebooks/baselines.py
+++ Omega-notebooks/baselines.py
@@ -14,13 +14,46 @@ class BaseLines:
             while not tmpenv.done:
                 if scheduler == 'SJF':
                     action = self.sjf_action(tmpenv.jobqueue)
+                elif scheduler == 'LJF':
+                    action = self.ljf_action(tmpenv.jobqueue)
                 elif scheduler == 'RANDOM':
                     action = self.random_action(tmpenv.jobqueue)
+                elif scheduler == 'TETRIS':
+                    action = self.tetris_action(tmpenv.resources, tmpenv.jobqueue)
+                # if scheduler == 'SJF':
+                #     action = self.sjf_action(tmpenv.jobqueue)
+                # elif scheduler == 'RANDOM':
+                #     action = self.random_action(tmpenv.jobqueue)
                 o,r,d=tmpenv.step(action)
 
             rewards[scheduler] = np.sum(tmpenv.episode_reward)
         return rewards
+    def tetris_action(self, resources, jobqueue):
+        num_available_gpus = np.count_nonzero(resources == -1)
 
+        jobs_status = [job.status for job in jobqueue]
+        jobs_gpu_request = [job.gpu_request for job in jobqueue]
+
+        indices_possible = [i for i in range(len(jobs_status)) if (jobs_status[i] != 'running') and (jobs_gpu_request[i] <= num_available_gpus)]
+
+        if len(indices_possible) == 0:
+            action = len(jobqueue)  # if no action available, hold
+        else:
+            gpu_request_max = max([jobs_gpu_request[i] for i in indices_possible])
+            action = jobs_gpu_request.index(gpu_request_max)
+
+        # if len(jobqueue) > 0:
+        #     lst = np.array([j.job_len for j in jobqueue])
+        #
+        #     def getter(j):
+        #         return j.status
+        #
+        #     vfunc = np.vectorize(getter, otypes=[str])
+        #     runnings = np.where(vfunc(jobqueue) == 'running')
+        #     lst[runnings] = 1000
+        #     action = np.argmin(lst)
+
+        return action
     def sjf_action(self, jobqueue):
         action = 0
 
@@ -37,6 +70,21 @@ class BaseLines:
 
         return action
 
+    def ljf_action(self, jobqueue):
+        action = 0  # 0 ~ 15
+
+        if len(jobqueue) > 0:
+            lst = np.array([j.job_len for j in jobqueue])
+
+            def getter(j):
+                return j.status
+
+            vfunc = np.vectorize(getter, otypes=[str])
+            runnings = np.where(vfunc(jobqueue) == 'running')
+            lst[runnings] = -1
+            action = np.argmax(lst)
+
+        return action
     def random_action(self, jobqueue):
         action = 0
 
